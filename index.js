3ead6b8fb1fad494be246d9ec26b78bbe592c48c
Benjamin Lotterer <theedgesofben@hotmail.com>
2023-11-17 14:14:10 +0100

    Fix set mainStorage to localStorage

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ else _fetch = fetch;
 if (typeof localStorage === 'undefined' || localStorage === null) {
     var LocalStorage = require('node-localstorage').LocalStorage;
     mainStorage = new LocalStorage('./scratch');
-}
+} else mainStorage = localStorage;
 
 if (typeof sessionStorage === 'undefined' || sessionStorage === null) {
     sessionStorage = require('sessionstorage');
